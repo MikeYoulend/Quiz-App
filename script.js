f338310d161f7bf40ca9e66f548c7c6cb26f403e
MikeYoulend <mikeyoulend@gmail.com>
2023-12-08 12:29:55 +0100

    Show Score

--- script.js
+++ script.js
@@ -94,4 +94,11 @@ function selectAnswer(e) {
 	nextButton.style.display = "block";
 }
 
+function showScore() {
+	resetState();
+	questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+	nextButton.innerHTML = "Play Again";
+	nextButton.style.display = "block";
+}
+
 startQuiz();
