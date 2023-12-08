dd7edb5d332372dc535c472e1c9ca7fa91cb40a1
MikeYoulend <mikeyoulend@gmail.com>
2023-12-08 12:29:35 +0100

    Select Answers

--- script.js
+++ script.js
@@ -76,4 +76,22 @@ function resetState() {
 	}
 }
 
+function selectAnswer(e) {
+	const selectedBtn = e.target;
+	const isCorrect = selectedBtn.dataset.correct === "true";
+	if (isCorrect) {
+		selectedBtn.classList.add("correct");
+		score++;
+	} else {
+		selectedBtn.classList.add("incorrect");
+	}
+	Array.from(answerButtons.children).forEach((button) => {
+		if (button.dataset.correct === "true") {
+			button.classList.add("correct");
+		}
+		button.disabled = true;
+	});
+	nextButton.style.display = "block";
+}
+
 startQuiz();
